e354fcc4c7cc70d615736a414d7ad42cb15edddf
thinh26 <thinh26@gitcommit.com>
2025-09-14 19:20:00 +0700

    add middleware url matching exception

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ acceptLanguage.languages(languages);
 export const config = {
   // matcher: '/:lng*'
   matcher: [
-    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)",
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|sw_prod.js|site.webmanifest).*)",
   ],
 };
 
