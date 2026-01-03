14bcf18c2d8acad803ca3a54a73ff57a9c0c93c0
thinh26 <thinh26@gitcommit.com>
2026-01-03 17:16:04 +0700

    fixing robots.txt #1

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,20 +1,10 @@
-import { personalData } from "@/data/data";
 import { MetadataRoute } from "next";
-import { headers } from "next/headers";
-
-export const dynamic = "force-dynamic";
 
 export default function robots(): MetadataRoute.Robots {
-  const hostname = headers().get("host")?.split(":").at(0);
-
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap:
-      process.env.NODE_ENV === "production"
-        ? `${hostname}/sitemap.xml`
-        : `http://${hostname}:3000/sitemap.xml`,
   };
 }
