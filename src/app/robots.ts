37ef67ea88ccc12e8879ccd7054c3400146dd313
thinh26 <thinh26@gitcommit.com>
2026-01-03 13:46:28 +0700

    fix robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import { personalData } from "@/data/data";
 import { MetadataRoute } from "next";
 import { headers } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export default function robots(): MetadataRoute.Robots {
   const hostname = headers().get("host")?.split(":").at(0);
 
