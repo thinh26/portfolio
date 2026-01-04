5be5db4c0cf70338286a4962b3690f124d722cbe
thinh26 <thinh26@gitcommit.com>
2026-01-04 16:46:07 +0700

    update sitemap last modified

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,12 +6,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? "";
   const fallbackUrl = process.env.NEXT_PUBLIC_SITEMAP_FALLBACK_URL ?? "";
   const baseModifiedTime = set(new Date(0), {
-    date: 3,
+    date: 4,
     month: 0, // 1-12 is 0-11
     year: 2026,
-    hours: 10,
-    minutes: 40,
-    seconds: 11,
+    hours: 16,
+    minutes: 45,
+    seconds: 7,
   }).toISOString();
   const baseDate = parseISO(baseModifiedTime);
   const lastmod = baseDate.toISOString();
